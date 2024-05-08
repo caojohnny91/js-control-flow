0d7ef059b264520e26d012e5e33896d93779071f
jcao91 <jcao91@yahoo.com>
2024-05-08 15:11:25 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,49 @@
+// let someNum = 5;
+
+// console.log(someNum >= 5);
+
+// if ("") {
+//     console.log("this is truthy);
+// }   else {
+//     console.log ("This is falsey");
+// }
+
+// const val =  4;
+// if (val === 1) {
+//     console.log("This value is one");
+// } else if (val === 2) {
+//     console.log("This value is two");
+// } else if (val === 3) {
+//     console.log("This value is three")
+// } else {
+//     console.log("val is not 1, 2, or 3")
+// }
+
+
+const color = 'green';
+if (color === 'green') 
+    console.log('Go');
+} else if (color === 'yellow') {
+    console.log('Slow');
+} else if (color === 'red') {
+    console.log('Stop');
+} else {
+    console.log('Whatever');
+}
+
+for (let i = 1; i < 10; i++) {
+    console.log('looping: ', i);
+}
+
+for (let number = 1; number < 10; number++) {
+    console.log(number);
+  }
+  for (let day = 1; day <= 7; day++) {
+    console.log(`Day ${day} of the week`);
+  }
+  
+
+
+for (let i = 1; i <= 20; i++) {
+    console.log(`${i} squared is ${i * i}`);
+}
